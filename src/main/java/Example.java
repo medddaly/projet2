85e7a38130c8a8afde1c9d71f0774df1149116de
medddaly <medddaly@gmail.com>
2018-01-31 21:55:00 +0100

    m4

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "bienvenue salut !";
+        return "salut !";
     }
 
     public static void main(String[] args) throws Exception {
